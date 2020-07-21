e495398f16ab636e6376c41d5ac7a6139519ef79
ValeraFo <68527733+ValeraFo@users.noreply.github.com>
2020-07-21 13:46:16 +0700

    Update core.py

--- core.py
+++ core.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import random
 import os
 
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '&')
 client.remove_command("help")
 
 @client.event
@@ -14,11 +14,11 @@ async def on_ready():
 async def help(ctx):
 	emb = discord.Embed(title = "Команды")
 	
-	emb.add_field( name = "!sum <num1> <num2>", value = "Суммирует num1 и num2")
-	emb.add_field( name = "!sub <num1> <num2>", value = "Отнимает num2 от num1")
-	emb.add_field( name = "!mul <num1> <num2>", value = "Умножает num1 на num2")
-	emb.add_field( name = "!div <num1> <num2>", value = "Делит num1 на num2")
-	emb.add_field( name = "!dice num1 num2", value = "Даёт случайное число от 1 до num1 и складывает его с num2")
+	emb.add_field( name = "&sum <num1> <num2>", value = "Суммирует num1 и num2")
+	emb.add_field( name = "&sub <num1> <num2>", value = "Отнимает num2 от num1")
+	emb.add_field( name = "&mul <num1> <num2>", value = "Умножает num1 на num2")
+	emb.add_field( name = "&div <num1> <num2>", value = "Делит num1 на num2")
+	emb.add_field( name = "&dice num1 num2", value = "Даёт случайное число от 1 до num1 и складывает его с num2")
 	
 	await ctx.send(embed = emb)
 
