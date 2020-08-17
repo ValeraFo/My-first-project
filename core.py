f720ff89d4ea802547c7e453ece5d1d74156f3d2
ValeraFo <68527733+ValeraFo@users.noreply.github.com>
2020-08-18 00:24:03 +0700

    Update core.py

--- core.py
+++ core.py
@@ -40,10 +40,6 @@ async def div(ctx,s1,s2):
 
 @client.command(pass_context = True)
 async def dice(ctx,dic,mod):
-'''
-	result = random.randint(1,int(dic))
-	await ctx.send(result+int(mod))
-'''
 	result = random.randint(1,int(dic))
 	try:
 		nam = str(result+int(mod))
